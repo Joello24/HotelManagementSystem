71c2f15cc2d0a22bf27fa5e0d8d1002dd4dfa0a8
Robert Lillyblad <robert.lillyblad@gmail.com>
2022-03-12 16:52:32 -0600

    Complete to specifications

---
+++ HotelController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListDemo
+{
+    static class MainClass
+    {
+        static void Main()
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            int roomNum = 0;
+            do
+            {
+                Console.Write("Enter the number of capsules: ");
+                int.TryParse(Console.ReadLine(), out roomNum); 
+            }
+            while (roomNum == 0);
+
+            string[] rooms = new string[roomNum];
+            Console.WriteLine($"There are {roomNum} unoccupied rooms ready to be booked.");
+            bool going = true;
+            while (going)
+            {
+                switch (MainMenu())
+                {
+                    case "1":
+                        CheckIn(rooms);
+                        break;
+                    case "2":
+                        CheckOut(rooms);
+                        break;
+                    case "3":
+                        ViewGuests(rooms);
+                        break;
+                    case "4":
+                        going = false;
+                        break;
+                }
+            }
+
+        }
+
+        private static string MainMenu()
+        {
+            Console.Write(@"Guest Menu
+        ==========
+        1. Check In
+        2. Check Out
+        3. View Guests
+        4. Exit
+        Choose on option [1-4]:");
+            return Console.ReadLine();
+        }
+
+        private static string[] CheckIn(string[] rooms) 
+        {
+            bool settingRoom = true;
+            Console.WriteLine("Guest Check In");
+            Console.WriteLine("===============");
+            Console.Write("Guest Name: ");
+            string guest = Console.ReadLine();
+
+            while (settingRoom) 
+            {
+                Console.Write($"Room #[0-{rooms.Length}]: ");
+                string newRoom = Console.ReadLine();
+                if (rooms[int.Parse(newRoom)] == "")
+                {
+                    Console.WriteLine($"Room #{newRoom} is occupied.");
+                }
+                else {
+                    Console.WriteLine("Success :) ");
+                    Console.WriteLine($"{guest} is booked in room #{newRoom}.");
+                    rooms[int.Parse(newRoom)] = guest;
+                    settingRoom = false;    
+                }
+            }
+            return rooms;
+        }
+        private static string[] CheckOut(string[] rooms)
+        {
+            bool checkingOut = true;
+            Console.WriteLine("Guest Check Out");
+            Console.WriteLine("===============");
+
+
+            while (checkingOut)
+            {
+                Console.Write($"Room #[0-{rooms.Length}]: ");
+                string roomNumber = Console.ReadLine();
+                if (rooms[int.Parse(roomNumber)] == "")
+                {
+                    Console.WriteLine($"Room #{roomNumber} is unoccupied.");
+                }
+                else
+                {
+                    Console.WriteLine("Success :) ");
+                    Console.WriteLine($"{rooms[int.Parse(roomNumber)]} checked out of room #{roomNumber}.");
+                    rooms[int.Parse(roomNumber)] = "";
+                    checkingOut = false;
+                }
+            }
+            return rooms;
+        }
+        private static void ViewGuests(string[] rooms)
+        {
+            Console.Write($"Room #[0-{rooms.Length}]: ");
+            Console.WriteLine();
+            int index;
+            do
+            {
+                Console.Write($"Room #[0-{rooms.Length}]: ");
+                int.TryParse(Console.ReadLine(), out index);
+            }
+            while (index == 0);
+            int lowindex = index - 5;
+            int highindex = index + 5;
+            if (lowindex < 0) {lowindex = 0;}
+            if (highindex > rooms.Length-1) { highindex = rooms.Length-1; }
+            for (int i = lowindex; i <= highindex; i++)
+            {
+                string tri = string.IsNullOrEmpty(rooms[i]) == true ? "[Unoccupied]" : rooms[i];
+                Console.WriteLine($"{i}: {tri}");
+            }
+        }
+    }
+}
